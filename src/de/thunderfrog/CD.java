140f955a7d4630774a3e998caeddaff8694477ec
VanKhaos <xv50k870!?>
2021-03-04 14:39:54 +0100

    add Setter & Getter

--- src/de/thunderfrog/CD.java
+++ src/de/thunderfrog/CD.java
@@ -3,6 +3,22 @@ package de.thunderfrog;
 public class CD{
     String artist, title;
 
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public CD(String title, String artist) {
         this.title = title;
         this.artist = artist;
@@ -11,4 +27,6 @@ public class CD{
     public void play(){
         System.out.println("Spiele " + artist + ", " + title);
     }
+
+
 }
