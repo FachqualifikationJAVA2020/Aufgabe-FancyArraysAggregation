26260db159d0b36ea584f464081a67c7f12b1f27
VanKhaos <xv50k870!?>
2021-03-04 12:24:34 +0100

    add Test

--- src/de/thunderfrog/Main.java
+++ src/de/thunderfrog/Main.java
@@ -1,8 +1,21 @@
 package de.thunderfrog;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            CDCollection collection=new CDCollection();
+            collection.addCD(new CD("Wonderwall","Oasis"));
+            collection.addCD(new CD("Galvanize","Chemical Brothers"));
+
+            Scanner keyboard= new Scanner(System.in);
+
+            String title=keyboard.nextLine(), artist=keyboard.nextLine();
+
+            if (collection.playCD(title,artist)==false) {
+                System.out.println("Diese CD Ist nicht in der Sammlung enthalten");
+            }
     }
+
 }
